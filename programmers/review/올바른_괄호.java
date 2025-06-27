adeb53bd6e38d782da62390c385c933a85fcc583
dbqudgns <dbqudgns4632@gmail.com>
2025-06-27 12:12:59 +0900

    dev : 프로그래머스 - 올바른 괄호 복습

---
+++ programmers/review/올바른_괄호.java
@@ -0,0 +1,41 @@
+package review;
+
+import java.util.*;
+
+public class 올바른_괄호 {
+
+    public static boolean solution(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ')' && stack.isEmpty()) {
+                return false;
+            }
+
+            else if (s.charAt(i) == ')') {
+                stack.pop();
+            }
+
+            else stack.add(s.charAt(i));
+
+        }
+
+        if (!stack.isEmpty()) {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(solution(s));
+    }
+}
