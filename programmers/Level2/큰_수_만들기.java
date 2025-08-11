ada3fc0305af71e1843d42b1f8900ca0babeb49b
dbqudgns <dbqudgns4632@gmail.com>
2025-08-11 17:16:19 +0900

    dev : 큰 수 만들기

---
+++ programmers/Level2/큰_수_만들기.java
@@ -0,0 +1,45 @@
+package Level2;
+
+import java.util.*;
+
+public class 큰_수_만들기 {
+
+    public static String solution(String number, int k) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < number.length(); i++) {
+            while(!stack.isEmpty() &&
+                    stack.peek() < number.charAt(i) &&
+                      k-- > 0) {
+                stack.pop();
+
+            }
+
+            stack.push(number.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        if (k > 0) {
+            return sb.reverse().toString().substring(0, sb.toString().length() - k);
+        }
+
+        return sb.reverse().toString();
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String number = sc.nextLine();
+        int k = sc.nextInt();
+
+        System.out.println(solution(number, k));
+    }
+
+}
