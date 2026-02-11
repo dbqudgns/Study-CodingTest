c4e91f99c10fa38ed4f8cf3cef60634274f884d1
dbqudgns <dbqudgns24@naver.com>
2026-02-12 06:31:03 +0900

    dev : N과M_6

---
+++ backjun/백트래킹/N과M_6.java
@@ -0,0 +1,62 @@
+package 백트래킹;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class N과M_6 {
+
+    static int N, M;
+    static int array[];
+    static int answer[];
+
+    static StringBuilder sb;
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(token.nextToken());
+        M = Integer.parseInt(token.nextToken());
+
+        array = new int[N];
+        answer = new int[M];
+        sb = new StringBuilder();
+
+        token = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(token.nextToken());
+        }
+
+        Arrays.sort(array);
+
+        solution(0, 0);
+
+        System.out.print(sb);
+    }
+
+    public static void solution(int startIndex, int depth) {
+
+        // 기저 조건
+        if (depth == M) {
+
+            for (int num : answer) {
+                sb.append(num).append(" ");
+            }
+
+            sb.append("\n");
+
+            return;
+        }
+
+        // 수행
+        for (int i = startIndex; i < N; i++) {
+            answer[depth] = array[i];
+            solution(i + 1, depth + 1);
+        }
+    }
+}
